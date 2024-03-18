6d304ab0f307e0c67cf722f3d6dd51e1415acc1d
clalamaciel <162983236+clalamaciel@users.noreply.github.com>
2024-03-18 12:10:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSkifjjijhyCfiL_eySZW_xKo_ltheX-uvNH4IsK2DjuCPeQdqJqYAoLiJX0HVKFJUAImN1M8cRhl_N/pub?output=csv')
-st.dataframe(voc)
 l=voc.shape[o]
 i=np.random.choice(range(l))
 word_fr=voc['Définition'].values[i]
