ca0e11172d48b607531dddd61d73bacb46724c8d
clalamaciel <162983236+clalamaciel@users.noreply.github.com>
2024-03-18 12:06:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as st
-name=st.text_input("Your name")
-st.write("Hello"+name)
-st.radio
+import pandas as pd
+import numpy as np
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSkifjjijhyCfiL_eySZW_xKo_ltheX-uvNH4IsK2DjuCPeQdqJqYAoLiJX0HVKFJUAImN1M8cRhl_N/pub?output=csv')
+st.dataframe(voc)
+l=voc.shape[o]
+i=np.random.choice(range(l))
+word_fr=voc['Définition'].values[i]
+word_chi=voc['?'].values[i]
+st.write(word_fr+"Hanzi"+word_chi)
+st.button("refresh")
